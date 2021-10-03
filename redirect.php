c017f43174c9884d30d223d91b964e98a193e06c
Ralph Euler Jean Baptiste <ralpheulerjb@Ralphs-MacBook-Pro.local>
2021-10-03 16:00:37 -0400

    fix dereferrer

--- redirect.php
+++ redirect.php
@@ -2,14 +2,16 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-
-<meta http-equiv="refresh" content="0; URL=<?php echo urldecode($url); ?>" />
 <title><?php echo urldecode($url); ?> &hellip;</title>
 </head>
 <body>
-<div align="center">
-  <p><?php echo urldecode($url); ?> &hellip;</p>
-</div>			
+<a style="display: none;" id="redlink" href="<?php echo urldecode($url); ?>" rel="noreferrer"></a>
 </body>
+
+<script>
+    setTimeout(function(){
+        document.getElementById('redlink').click();
+    })
+</script>
 
 </html>
